beacc825870429c2c8e290f5b9606c88876e1915
arounamounchili <virtual.power.virtual@gmail.com>
2024-01-20 10:42:55 +0100

    j'ai supprimer certaines ligne

--- 100_test.cpp
+++ 100_test.cpp
@@ -1,26 +1,7 @@
-/*
-    Notre Premier Programme: Tout fichier C++ doivent avoir l'extension .cpp
-    Exemple: helloWorld.cpp
-*/
 
-// Cette ligne permet d'inclure la bbliothèque "iostream" du C++
-// Pour utiliser les fonctionalités du langage
 #include <iostream>
-
-// on va utilisé l'espace de nom std.
 using namespace std;
 
-// fonction principale: tous les programme doivent possède une fonction main
 int main()
 {
-    // Notre premiere instruction: affiecher un message a l'ecran
-    cout << "Hello World! \n"; // la directive cout demande a la machine
-                               // d'afficher un message à l'ecran
-
-    cout << "Welcome to Minteligencia Community";
-
-    cout << "Hello World\nWelcome to Minteligencia Community\n";
-
-    // Ce type d'instrustion clot generalemenent les fonctions.
-    return 0; // la fonction main renvoit 0 pour indiquer que tout s'est bien passé.
 }
