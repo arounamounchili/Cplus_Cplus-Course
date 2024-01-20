630fc9b9c46a2b0075165bcf6aa73af21b371def
arounamounchili <virtual.power.virtual@gmail.com>
2024-01-20 11:24:00 +0100

    add function division

--- 100_test.cpp
+++ 100_test.cpp
@@ -2,7 +2,15 @@
 #include <iostream>
 using namespace std;
 
+float division(float a, float b);
+
 int main()
 {
-  std::cout << "Hello world!" << endl;
+    std::cout << "Welconne to Minteligencia!" << endl;
+    cout << "Le resultat de la division est: " << division(12.0, 4.0) << endl;
+}
+
+float division(float a, float b)
+{
+    return a / b;
 }
