902938fa5322a7a44cebf966916b9bcff63bb7b5
Gueche <nkwonkamabraham@yahoo.fr>
2024-01-20 11:47:25 +0100

    add fonction  soustraction

--- 100_test.cpp
+++ 100_test.cpp
@@ -2,7 +2,27 @@
 #include <iostream>
 using namespace std;
 
+double soustraction (double a , double b){
+
+return a-b ; 
+
+}
+
 int main()
 {
-  std::cout << "Hello world!" << endl;
+  //std::cout << "Hello world!" << endl;
+  double x ;
+  double y; 
+
+  cout << "entre le numbre 1 " << endl; 
+  cin >> x ; 
+
+  cout << "entre le nombre 2 " << endl ;
+  cin>> y ; 
+  
+  double resultat = soustraction (x,y); 
+
+  cout << " le resultat est : " << resultat << endl ;
 }
+
+
