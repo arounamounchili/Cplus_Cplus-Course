ef0dde422bdbb972408093389d941d924efa29e2
arounamounchili <virtual.power.virtual@gmail.com>
2024-01-27 10:31:25 +0100

    accept merge

--- 100_test.cpp
+++ 100_test.cpp
@@ -2,47 +2,32 @@
 #include <iostream>
 using namespace std;
 
-double soustraction (double a , double b){
+double soustraction(double a, double b)
+{
 
-return a-b ; 
-
+    return a - b;
 }
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 12a9c7e8591f457e3e0a3dc5c37bbf7d6949c726
 float division(float a, float b)
 {
     return a / b;
 }
 
-<<<<<<< HEAD
-int addition(int number1, int number2) 
-{
-  return number1 + number2;
-}
-=======
 int main()
 {
-  //std::cout << "Hello world!" << endl;
-  double x ;
-  double y; 
+    // std::cout << "Hello world!" << endl;
+    double x;
+    double y;
 
-  cout << "entre le numbre 1 " << endl; 
-  cin >> x ; 
+    cout << "entre le numbre 1 " << endl;
+    cin >> x;
 
-  cout << "entre le nombre 2 " << endl ;
-  cin>> y ; 
-  
-  double resultat = soustraction (x,y); 
+    cout << "entre le nombre 2 " << endl;
+    cin >> y;
 
-  cout << " le resultat est : " << resultat << endl ;
+    double resultat = soustraction(x, y);
 
-   std::cout << "Welconne to Minteligencia!" << endl;
+    cout << " le resultat est : " << resultat << endl;
+
+    std::cout << "Welconne to Minteligencia!" << endl;
     cout << "Le resultat de la division est: " << division(12.0, 4.0) << endl;
-  
 }
-
-
->>>>>>> 12a9c7e8591f457e3e0a3dc5c37bbf7d6949c726
